689f5d88ef090f1f89e626c3f1f5d070a4759c30
Ruben <rubendc91@gmail.com>
2022-11-29 13:23:37 -0800

    change git ignore

---
+++ src/App.js
@@ -0,0 +1,39 @@
+import React, { useState } from 'react';
+import Footer from './components/Footer';
+import Header from './components/Header';
+import Nav from './components/Nav';
+import Page from './components/Page';
+// import PageContent from './components/PageContent';
+
+
+function App() {
+  const [pages] = useState([
+    {
+      name: "About"
+    },
+    {
+      name: "Merch"
+    },
+    {
+      name: "Contact"
+    },
+    {
+      name: "Info"
+    },
+  ])
+
+  const [currentPage, setCurrentPage] = useState(pages[0])
+  return (
+    <div>
+      <Header>
+        <Nav pages={pages} setCurrentPage={setCurrentPage} currentPage={currentPage} />
+      </Header>
+
+      <main>
+        <Page currentPage={currentPage}></Page>
+      </main>
+      <Footer />
+    </div>
+  );
+}
+export default App;
