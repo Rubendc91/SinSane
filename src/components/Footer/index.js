ef404b2d721fa2cd607b427bb9df1e9d0e48db60
Rubendc91 <Rubendc91@yahoo.com>
2023-06-05 22:01:41 -0700

    added fatlip to footer section and styled

--- src/components/Footer/index.js
+++ src/components/Footer/index.js
@@ -19,37 +19,68 @@ function Footer() {
 
     ]
 
+    const iconsFatLip = [
+            {
+                name: "fa-brands fa-instagram",
+                links: "https://www.instagram.com/omnitheartist/?hl=en"
+            },
+            {
+                name: "fa-brands fa-tiktok",
+                links: "https://www.tiktok.com/@omnitheartist"
+            },
+            {
+                name: "fa-brands fa-facebook",
+                links: "https://www.facebook.com/Omnisplayground"
+            }
+    
+    
+        ]
+
 
     const linkStyle = {
         textDecoraton: "none",
-        color: "black",
-        cursor: "pointer"
+        cursor: "pointer",
+        alignSelf: "center",
+
     }
 
     const FooterStyle = {
         height: "50px",
-        // backgroundColor: "white",
         display: "grid",
-        alignItems: "center",
         justifyContent: "center",
         gridGap: "15px"
 
     }
     const FooterSection = {
-        display: "grid",
-        gridTemplateColumns: "max-content 1fr 1fr 1fr 1fr",
-        gridGap: "20px"
+        display:"flex",
+        gridGap: "20px",
+
     }
 
 
     return (
         <footer style={FooterStyle} className="footer">
-            <section style={FooterSection} >
-                <section style={{ paddingRight: "15px"}}>© {new Date().getFullYear()}</section>
-                {iconsOmni.map(icon => {
+            <section style={FooterSection} className="footer">
+                <section style={FooterStyle} className="footer">
+                    <section style={FooterSection} className="footer">
+                        <p>Omni</p>    
+                    {iconsOmni.map(icon => {
+                        return <a href={icon.links} key={icon.name} target="_blank" rel="noopener noreferrer" style={linkStyle}><i className={icon.name}></i></a>
+                    })}
+                    </section>
+                </section>
+                <section style={FooterStyle}>
+                    <section style={FooterSection}>
+                    <p>Fat Lip</p>    
+                    {iconsFatLip.map(icon => {
                     return <a href={icon.links} key={icon.name} target="_blank" rel="noopener noreferrer" style={linkStyle}><i className={icon.name}></i></a>
                 })}
+                </section>
+                </section>
             </section>
+            <section style={FooterStyle}>
+                <section>Sinsane © {new Date().getFullYear()}</section>
+                </section>
         </footer>
     )
 
